ebb339ff0b33fcdc4cb3d4338c7f6b9fc7a87c52
maaarcus <40426362+maaarcus@users.noreply.github.com>
2019-04-06 20:43:00 +0800

    Update controlAirCon.js

--- controlAirCon.js
+++ controlAirCon.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const shell = require('shelljs');
 
 let {PythonShell} = require('python-shell')
 var admin = require("firebase-admin");
@@ -26,6 +27,7 @@ var ref = admin.database().ref("airCon");
 
 ref.on("value", function(snapshot) {
   console.log(snapshot.val());
+  shell.exec(' ~/MarcusWork/turn_on_aircon.sh')
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
