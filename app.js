f8c9258941ce2b009e8960785e39f67be0b74245
maaarcus <40426362+maaarcus@users.noreply.github.com>
2019-04-07 14:45:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -28,10 +28,36 @@ function sleep(milliseconds) {
   }
 }
 
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
+
 function updateFirebase(temp,humid) {
   admin.database().ref('dht22').set({
     humi: humid,
     temp: temp,
+    last_update: getDateTime(),
   });
 }
 
